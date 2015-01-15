cbc88bed09b5a30a119d032e7040d783317518c3
Matt Liegey <mtliegey@gmail.com>
2015-01-15 17:50:33 -0500

    using browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp')
   , jade = require('gulp-jade')
   , sass = require('gulp-sass')
+  , browserify = require('gulp-browserify')
   ;
 
 gulp.task('templates', function () {
@@ -23,9 +24,23 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(process.cwd() + '/www/css'));
 });
 
+gulp.task('scripts', function() {
+  gulp.src(process.cwd() + '/src/js/*.js')
+    .pipe(browserify({
+      insertGlobals : true,
+      debug         : !gulp.env.production
+    }))
+    .pipe(gulp.dest(process.cwd() + '/www/js'));
+});
+
 gulp.task('watch', function () {
   gulp.watch(process.cwd() + '/src/templates/**/*.jade', ['templates']);
   gulp.watch(process.cwd() + '/src/scss/*.scss',         ['sass']);
+  gulp.watch(process.cwd() + '/src/js/*.js',             ['scripts']);
 });
 
-gulp.task('build', ['templates', 'sass']);
+gulp.task('build', ['templates', 'sass', 'scripts']);
+
+
+
+var gulp = require('gulp');
